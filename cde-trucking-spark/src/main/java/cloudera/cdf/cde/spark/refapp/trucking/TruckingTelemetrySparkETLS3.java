be6d31161f7bcfbd645bdb8d5b3388e2f6fc8b2b
georgevetticaden <george.vetticaden@gmail.com>
2021-03-15 09:54:11 -0500

    adding S3 support

--- cde-trucking-spark/src/main/java/cloudera/cdf/cde/spark/refapp/trucking/TruckingTelemetrySparkETLS3.java
+++ cde-trucking-spark/src/main/java/cloudera/cdf/cde/spark/refapp/trucking/TruckingTelemetrySparkETLS3.java
@@ -10,15 +10,22 @@ import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.functions;
 import org.apache.spark.sql.functions.*;
 import org.apache.spark.sql.types.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TruckingTelemetrySparkETLS3 {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(TruckingTelemetrySparkETLS3.class);
 	
 	
 	public static void main(String[] args) throws AnalysisException {
 		// $example on:init_session$
 		
-		String sourceFile = args[0];
+		String s3Folder = args[0];
 		String destination = args[1];
+		
+		String accessKey = args[2];
+		String accessPassword = args[3];
 		
 		SparkSession spark = SparkSession.builder()
 				.appName("Java Spark SQL basic example")
@@ -26,13 +33,26 @@ public class TruckingTelemetrySparkETLS3 {
 
 		spark.sparkContext().setLogLevel("WARN");
 		
-		truckingTelemetryETL(spark, sourceFile, destination);
+		// Replace Key with your AWS account key (You can find this on IAM 
+		spark.sparkContext().hadoopConfiguration().set("fs.s3a.access.key", accessKey);
+		spark.sparkContext().hadoopConfiguration().set("fs.s3a.secret.key", accessPassword);
+		spark.sparkContext().hadoopConfiguration().set("s.s3a.endpoint", "s3.amazonaws.com");
+		
+
+		truckingTelemetryETL(spark, s3Folder, destination);
 
 		spark.stop();
 	}
 
-	private static void truckingTelemetryETL(SparkSession spark, String sourceFile, String destinationFile) {
-		Dataset<Row> telemetryDataSet = spark.read().json(sourceFile);
+	private static void truckingTelemetryETL(SparkSession spark, String s3Folder, String destinationFile) {
+		String s3FolderLocationUrl = "s3a://"+ s3Folder + "/*";
+		
+		LOG.warn("S3 Folder Url is: " + s3FolderLocationUrl);
+		
+		Dataset<Row> telemetryDataSet = spark.read().json(s3FolderLocationUrl);
+		
+		LOG.warn("Number of records is: " + telemetryDataSet.count());
+		
 		debug(telemetryDataSet, "Raw Data Set");
 		
 		/* Extract Time Components for partitions when writing to Impala */
@@ -48,10 +68,29 @@ public class TruckingTelemetrySparkETLS3 {
 		Dataset<Row> truckSpeedEvents =  
 				telemetryDataSet.filter(col("eventSource").equalTo("truck_speed_event"))
 								.drop("eventType", "longitude", "latitude", "correlationId");
-		debug(truckSpeedEvents, "Truck Speed Event Telemetry");
 		
-		/* Write telemetry data to destination */
-		truckGeoEvents.write().mode("append").partitionBy("truckId", "year", "month", "day", "hour").parquet(destinationFile);
+		
+		long speedEventsCount = truckSpeedEvents.count();
+		
+		/* filter for Speed Events greater than 60 */
+		
+		// Register DataFrame as as SQL temporary view
+		truckSpeedEvents.createOrReplaceTempView("truck_speed_events");
+		Dataset<Row> fileredTruckSpeedEvents =  spark.sql("select * from truck_speed_events where speed > 60");
+		debug(fileredTruckSpeedEvents, "Truck Speed Event Telemetry Filtered");
+		
+		long speedEventsFilteredCount = fileredTruckSpeedEvents.count();
+		LOG.warn("Before filter, speed events count is: " + speedEventsCount + ". After Filter, count is: " + speedEventsFilteredCount);
+		
+		/* Write geo data to destination in avro Format*/
+		String geoDestFile = destinationFile + "/truck_geo_events";
+		truckGeoEvents.write().mode("append").partitionBy("truckId", "year", "month", "day", "hour").parquet(geoDestFile);
+		
+		/* Write speed data to destination in avro Format*/
+		String speedDestFile = destinationFile + "/truck_speed_events";
+		fileredTruckSpeedEvents.write().mode("append").partitionBy("truckId", "year", "month", "day", "hour").parquet(speedDestFile);
+		
+		
 		
 	}
 
