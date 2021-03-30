be6b658628acf8c9e7621f0ddc8eeeab274a91d8
georgevetticaden <george.vetticaden@gmail.com>
2021-03-30 08:16:22 -0500

    cleanup

--- csp-trucking-kafka/src/main/resources/scripts/smm/createKafkaTopics.sh
+++ csp-trucking-kafka/src/main/resources/scripts/smm/createKafkaTopics.sh
@@ -1,288 +1,242 @@
 #!/bin/bash
 if [ $# -ne 1 ]; then
-        echo "Please pass ZK:Port to run as the first argument."
+        echo "Please pass Broker:Port to run as the first argument."
 else
 	/usr/bin/kafka-topics \
 	--create \
-	--zookeeper $1 \
+	--bootstrap-server $1 \
 	--replication-factor 2 \
 	--partitions 3 \
 	--topic gateway-west-raw-sensors;
 
 	/usr/bin/kafka-topics \
 	--create \
-	--zookeeper $1 \
+	--bootstrap-server $1 \
 	--replication-factor 2 \
 	--partitions 3 \
 	--topic gateway-central-raw-sensors;
 
 	/usr/bin/kafka-topics \
 	--create \
-	--zookeeper $1 \
+	--bootstrap-server $1 \
 	--replication-factor 2 \
 	--partitions 3 \
 	--topic gateway-east-raw-sensors;
 	
 	/usr/bin/kafka-topics \
 	--create \
-	--zookeeper $1 \
+	--bootstrap-server $1 \
 	--replication-factor 2 \
 	--partitions 3 \
 	--topic gateway-europe-raw-sensors;	
 
 	/usr/bin/kafka-topics \
 	--create \
-	--zookeeper $1 \
+	--bootstrap-server $1 \
 	--replication-factor 2 \
 	--partitions 3 \
 	--topic syndicate-geo-event-avro;
 
 	/usr/bin/kafka-topics \
 	--create \
-	--zookeeper $1 \
+	--bootstrap-server $1 \
 	--replication-factor 2 \
 	--partitions 3 \
 	--topic syndicate-speed-event-avro;
 
 	/usr/bin/kafka-topics \
 	--create \
-	--zookeeper $1 \
+	--bootstrap-server $1 \
 	--replication-factor 2 \
 	--partitions 3 \
 	--topic syndicate-geo-event-json;
 
 	/usr/bin/kafka-topics \
 	--create \
-	--zookeeper $1 \
+	--bootstrap-server $1 \
 	--replication-factor 2 \
 	--partitions 3 \
 	--topic syndicate-speed-event-json;
 
 	/usr/bin/kafka-topics \
 	--create \
-	--zookeeper $1 \
+	--bootstrap-server $1 \
 	--replication-factor 2 \
 	--partitions 3 \
 	--topic alerts-speeding-drivers;
 	
 	/usr/bin/kafka-topics \
 	--create \
-	--zookeeper $1 \
+	--bootstrap-server $1 \
 	--replication-factor 2 \
 	--partitions 3 \
 	--topic syndicate-oil;	
 	
 	/usr/bin/kafka-topics \
 	--create \
-	--zookeeper $1 \
+	--bootstrap-server $1 \
 	--replication-factor 2 \
 	--partitions 3 \
 	--topic syndicate-battery;
 	
 	/usr/bin/kafka-topics \
 	--create \
-	--zookeeper $1 \
+	--bootstrap-server $1 \
 	--replication-factor 2 \
 	--partitions 3 \
 	--topic syndicate-transmission;
 	
 	/usr/bin/kafka-topics \
 	--create \
-	--zookeeper $1 \
+	--bootstrap-server $1 \
 	--replication-factor 2 \
 	--partitions 3 \
 	--topic syndicate-all-geo-critical-events;
 	
 	/usr/bin/kafka-topics \
 	--create \
-	--zookeeper $1 \
+	--bootstrap-server $1 \
 	--replication-factor 2 \
 	--partitions 3 \
 	--topic fleet-supply-chain;	
 	
 	/usr/bin/kafka-topics \
 	--create \
-	--zookeeper $1 \
+	--bootstrap-server $1 \
 	--replication-factor 2 \
 	--partitions 3 \
 	--topic route-planning ;	
 	
 	/usr/bin/kafka-topics \
 	--create \
-	--zookeeper $1 \
+	--bootstrap-server $1 \
 	--replication-factor 2 \
 	--partitions 3 \
 	--topic load-optimization ;		
 	
 	/usr/bin/kafka-topics \
 	--create \
-	--zookeeper $1 \
+	--bootstrap-server $1 \
 	--replication-factor 2 \
 	--partitions 3 \
 	--topic fuel-logistics ;
 	
 	/usr/bin/kafka-topics \
 	--create \
-	--zookeeper $1 \
+	--bootstrap-server $1 \
 	--replication-factor 2 \
 	--partitions 3 \
 	--topic supply-chain ;	
 	
 	/usr/bin/kafka-topics \
 	--create \
-	--zookeeper $1 \
+	--bootstrap-server $1 \
 	--replication-factor 2 \
 	--partitions 3 \
 	--topic predictive-alerts ;
 	
 	/usr/bin/kafka-topics \
 	--create \
-	--zookeeper $1 \
+	--bootstrap-server $1 \
 	--replication-factor 2 \
 	--partitions 3 \
 	--topic energy-mgmt ;	
 	
 	/usr/bin/kafka-topics \
 	--create \
-	--zookeeper $1 \
+	--bootstrap-server $1 \
 	--replication-factor 2 \
 	--partitions 3 \
 	--topic audit-events ;		
 	
 	/usr/bin/kafka-topics \
 	--create \
-	--zookeeper $1 \
+	--bootstrap-server $1 \
 	--replication-factor 2 \
 	--partitions 3 \
 	--topic compliance ;
 	
 	/usr/bin/kafka-topics \
 	--create \
-	--zookeeper $1 \
+	--bootstrap-server $1 \
 	--replication-factor 2 \
 	--partitions 3 \
 	--topic adjudication ;
 	
 	/usr/bin/kafka-topics \
 	--create \
-	--zookeeper $1 \
+	--bootstrap-server $1 \
 	--replication-factor 2 \
 	--partitions 3 \
 	--topic approval ;	
 	
 	/usr/bin/kafka-topics \
     --create \
-    --zookeeper $1 \
+    --command-config client.properties \
+    --bootstrap-server $1 \
     --replication-factor 2 \
     --partitions 3 \
     --topic driver-violation-events;	
     
      /usr/bin/kafka-topics \
     --create \
-    --zookeeper $1 \
+    --command-config client.properties \
+    --bootstrap-server $1 \
     --replication-factor 2 \
     --partitions 3 \
     --topic driver-average-speed;
     
      /usr/bin/kafka-topics \
     --create \
-    --zookeeper $1 \
+    --command-config client.properties \
+    --bootstrap-server $1 \
     --replication-factor 2 \
     --partitions 3 \
     --topic gateway-geo-raw-sensor;    
     
      /usr/bin/kafka-topics \
     --create \
-    --zookeeper $1 \
+    --command-config client.properties \
+    --bootstrap-server $1 \
     --replication-factor 2 \
     --partitions 3 \
     --topic gateway-speed-raw-sensor;      
     
-     	/usr/bin/kafka-topics \
-        --create \
-        --zookeeper $1 \
-        --replication-factor 2 \
-        --partitions 3 \
-        --topic gateway-freightliner-sensors;
+     /usr/bin/kafka-topics \
+    --create \
+    --command-config client.properties \
+    --bootstrap-server $1 \
+    --replication-factor 2 \
+    --partitions 3 \
+    --topic speed-events-filtered;         	 
+    
+	/usr/bin/kafka-topics \
+	--create \
+	--bootstrap-server $1 \
+	--replication-factor 2 \
+	--partitions 3 \
+	--topic gateway-west-raw-sensors-json;
 
-        /usr/bin/kafka-topics \
-        --create \
-        --zookeeper $1 \
-        --replication-factor 2 \
-        --partitions 3 \
-        --topic gateway-tata-sensors;
+	/usr/bin/kafka-topics \
+	--create \
+	--bootstrap-server $1 \
+	--replication-factor 2 \
+	--partitions 3 \
+	--topic gateway-central-raw-sensors-json;
 
-        /usr/bin/kafka-topics \
-        --create \
-        --zookeeper $1 \
-        --replication-factor 2 \
-        --partitions 3 \
-        --topic gateway-mack-sensors;
-
-        /usr/bin/kafka-topics \
-        --create \
-        --zookeeper $1 \
-        --replication-factor 2 \
-        --partitions 3 \
-        --topic gateway-benz-sensors;
-
-        /usr/bin/kafka-topics \
-        --create \
-        --zookeeper $1 \
-        --replication-factor 2 \
-        --partitions 3 \
-        --topic syndicate-tata-speed;
-
-        /usr/bin/kafka-topics \
-        --create \
-        --zookeeper $1 \
-        --replication-factor 2 \
-        --partitions 3 \
-        --topic syndicate-tata-geo;
-
-        /usr/bin/kafka-topics \
-        --create \
-        --zookeeper $1 \
-        --replication-factor 2 \
-        --partitions 3 \
-        --topic syndicate-mack-speed;
-        
-
-        /usr/bin/kafka-topics \
-        --create \
-        --zookeeper $1 \
-        --replication-factor 2 \
-        --partitions 3 \
-        --topic syndicate-mack-geo;
-
-        /usr/bin/kafka-topics \
-        --create \
-        --zookeeper $1 \
-        --replication-factor 2 \
-        --partitions 3 \
-        --topic syndicate-freightliner-speed;
-
-        /usr/bin/kafka-topics \
-        --create \
-        --zookeeper $1 \
-        --replication-factor 2 \
-        --partitions 3 \
-        --topic syndicate-freightliner-geo;
-
-        /usr/bin/kafka-topics \
-        --create \
-        --zookeeper $1 \
-        --replication-factor 2 \
-        --partitions 3 \
-        --topic syndicate-benz-speed;
-
-        /usr/bin/kafka-topics \
-        --create \
-        --zookeeper $1 \
-        --replication-factor 2 \
-        --partitions 3 \
-        --topic syndicate-benz-geo;
-       	    
+	/usr/bin/kafka-topics \
+	--create \
+	--bootstrap-server $1 \
+	--replication-factor 2 \
+	--partitions 3 \
+	--topic gateway-east-raw-sensors-json;
+	
+	/usr/bin/kafka-topics \
+	--create \
+	--bootstrap-server $1 \
+	--replication-factor 2 \
+	--partitions 3 \
+	--topic gateway-europe-raw-sensors-json;	       
        	
 				
 fi
