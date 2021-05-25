bb2b3db695d2a363097540cc032db8ea2148bc47
georgevetticaden <george.vetticaden@gmail.com>
2021-05-25 15:53:22 -0500

    reference data

--- csp-trucking-flink/src/main/resources/reference-data/ddl/phoenix_create_timesheet.sql
+++
@@ -1,8 +0,0 @@
-drop table if exists timesheet;
-
-create table timesheet (
- driverid bigint not null,
- week bigint not null,
- hours_logged bigint,
- miles_logged bigint,
- CONSTRAINT pk PRIMARY KEY (driverid,week));
