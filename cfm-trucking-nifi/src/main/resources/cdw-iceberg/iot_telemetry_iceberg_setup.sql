b6fa10a11e8beabefaff5941dc00334b91fba848
gvetticaden <gvetticaden@0.0.88.207>
2023-03-03 01:06:33 -0600

    iceberg flows

--- cfm-trucking-nifi/src/main/resources/cdw-iceberg/iot_telemetry_iceberg_setup.sql
+++ cfm-trucking-nifi/src/main/resources/cdw-iceberg/iot_telemetry_iceberg_setup.sql
@@ -3,8 +3,17 @@ create database iot_telemetry;
 CREATE  TABLE iot_telemetry.geo_location_telemetry
 (eventtime string, eventtimelong bigint, eventsource string, truckid int, drivername string, 
 routeid int, route string, eventtype string, 
-latitude double, longitude double, correlationid bigint, 
-geoaddress string
+latitude double, longitude double, correlationid bigint
 )
 PARTITIONED BY (driverId int)
 STORED BY ICEBERG;
+
+
+describe extended iot_telemetry.geo_location_telemetry;
+
+select * from iot_telemetry.geo_location_telemetry where eventtype != 'Normal'
+
+select count(*) from iot_telemetry.geo_location_telemetry;
+
+
+drop table iot_telemetry.geo_location_telemetry;
